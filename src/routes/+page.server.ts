d9c11bc1089059a518aa90271f57cb3139014d1a
Parsa Estakhri <parsaestakhry03@gmail.com>
2024-05-22 23:38:09 +0330

    passed Data

--- src/routes/+page.server.ts
+++ src/routes/+page.server.ts
@@ -1,13 +1,34 @@
 import { GetCars } from '$lib/server/GetCars';
 import { GetUsers } from '$lib/server/GetUsers';
+import type { PageServerLoad } from './$types';
 
-(async () => {
+export type Car = {
+	type : string,
+	Color : string,
+	fuel_type : string,
+	id : number,
+	model : string,
+	make : string,
+	milage : number,
+	make_date : Date,
+	date_added : Date,
+	engine_model : string,
+	gearbox : string
+
+}
+
+export const load: PageServerLoad = async () => {
+    const cars = await GetCars();
 	try {
-		let carInfo = await GetCars();
-		console.log(carInfo);
+		// Return the fetched cars data as a property to be used in the component
+		return {
+			props: {
+				data:cars
+			}
+		};
 	} catch (error) {
-		console.error('Error fetching car data:', error);
+		console.error('Error loading data:', error);
+		// If an error occurs, return an empty object or handle the error as needed
+		return {};
 	}
-})();
-
-
+};
---
+++ src/routes/+page.svelte
@@ -0,0 +1,15 @@
+<!-- CarPage.svelte -->
+<script lang="ts">
+    import type { PageData } from "./$types";
+	import type { Car } from "./+page.server.js";
+    export let data : PageData;
+    
+</script>
+
+
+
+
+
+<h1>Cars Page</h1>
+
+
