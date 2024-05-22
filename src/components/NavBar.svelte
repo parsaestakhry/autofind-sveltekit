8bf26f7609324ef44d3a081ce4c5094af8444e8a
Parsa Estakhri <parsaestakhry03@gmail.com>
2024-05-22 19:48:52 +0330

    css and customization

--- src/components/NavBar.svelte
+++ src/components/NavBar.svelte
@@ -1,11 +1,11 @@
-<div class="navbar bg-base-100">
+<div class="navbar bg-black rounded-md">
   <div class="flex-none">
     <button class="btn btn-square btn-ghost">
       <svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" class="inline-block w-5 h-5 stroke-current"><path stroke-linecap="round" stroke-linejoin="round" stroke-width="2" d="M4 6h16M4 12h16M4 18h16"></path></svg>
     </button>
   </div>
   <div class="flex-1">
-    <a href="/" class="btn btn-ghost text-xl">daisyUI</a>
+    <a href="/" class="btn btn-ghost text-xl">autofind</a>
   </div>
   <div class="flex-none">
     <button class="btn btn-square btn-ghost">
