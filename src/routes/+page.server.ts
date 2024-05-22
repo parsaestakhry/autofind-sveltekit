c5993ea1776d394b28ae32c840248918115dfac7
Parsa Estakhri <parsaestakhry03@gmail.com>
2024-05-22 18:24:31 +0330

    rename for page.server

--- src/routes/+page.server.ts
+++ src/routes/+page.server.ts
@@ -2,7 +2,7 @@ import { connection } from '$lib/db/mysql';
 
 export async function load() {
 	let results = await connection.query('SELECT * FROM user').then(function ([rows, fields]) {
-		// console.log(rows);
+		//console.log(rows);
 		return rows;
 	});
 
