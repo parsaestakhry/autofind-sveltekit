e1e9b74ebc79cc481e889d4be30835e70e67b3ef
Parsa Estakhri <parsaestakhry03@gmail.com>
2024-05-23 14:51:10 +0330

    css for page

--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -1,19 +1,19 @@
 <!-- CarPage.svelte -->
 <script lang="ts">
 	import type { Car } from '$lib/server/GetCars';
+	import Card from '../components/Card.svelte';
 	import type { PageData } from './$types';
 	export let data: PageData;
 	const carArray: Car[] | undefined = data.props?.data;
-    console.log(carArray)
 </script>
 
-<h1>Cars Page</h1>
+
 
 {#if carArray && carArray.length > 0}
-    <ul>
+    <ul class="flex flex-wrap justify-center mt-2">
         {#each carArray as car}
             <li>
-                <strong>{car.make} {car.model}</strong> ({car.year_make})
+                <Card model={car.model} make={car.make} milage={car.milage} color={car.Color} fuel_type={car.fuel_type} year_make={car.year_make}/>
             </li>
         {/each}
     </ul>
