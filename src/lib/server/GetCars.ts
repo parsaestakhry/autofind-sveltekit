bf4bb99c374e0430d5df9ca7115faa79a3c579de
Parsa Estakhri <parsaestakhry03@gmail.com>
2024-05-23 16:59:26 +0330

    LIMIT 10

--- src/lib/server/GetCars.ts
+++ src/lib/server/GetCars.ts
@@ -13,7 +13,7 @@ export type Car = {
 	gearbox: string;
 };
 export const GetCars = async () => {
-	let results: Car[] = await connection.query('SELECT * FROM car LIMIT 5').then(function ([rows, fields]) {
+	let results: Car[] = await connection.query('SELECT * FROM car LIMIT 10').then(function ([rows, fields]) {
         //console.log(rows)
 		return rows as Car[];
 	});
