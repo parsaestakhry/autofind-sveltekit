13a8f3e952f19872c22eec2e0b8215ab1a51c5f9
Parsa Estakhri <parsaestakhry03@gmail.com>
2024-05-23 09:55:10 +0330

    type fix

--- src/lib/server/GetCars.ts
+++ src/lib/server/GetCars.ts
@@ -1,9 +1,21 @@
 import { connection } from '$lib/db/mysql';
-
+export type Car = {
+	type: string;
+	Color: string;
+	fuel_type: string;
+	id: number;
+	model: string;
+	make: string;
+	milage: number;
+	make_date: Date;
+	date_added: Date;
+	engine_model: string;
+	gearbox: string;
+};
 export const GetCars = async () => {
-	let results = await connection.query('SELECT * FROM car').then(function ([rows, fields]) {
+	let results: Car[] = await connection.query('SELECT * FROM car LIMIT 5').then(function ([rows, fields]) {
         //console.log(rows)
-		return rows;
+		return rows as Car[];
 	});
 
 	return {
