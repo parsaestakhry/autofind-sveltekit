e287bca65a080b8d666531b0f92a2244f9bd5894
Parsa Estakhri <parsaestakhry03@gmail.com>
2024-05-24 16:40:53 +0330

    implementing store

---
+++ src/store/store.ts
@@ -0,0 +1,7 @@
+import { writable } from 'svelte/store';
+
+export const email = writable<string>('');
+export const password = writable<string>('');
+export const passwordConfirm = writable<string>('');
+export const firstName = writable<string>('');
+export const lastName = writable<string>();
