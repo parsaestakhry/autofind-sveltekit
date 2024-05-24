79fe316f393a223f929c9f9f8f964d95bee62783
Parsa Estakhri <parsaestakhry03@gmail.com>
2024-05-24 12:12:57 +0330

    implemented Drawer

---
+++ src/components/Drawer.svelte
@@ -0,0 +1,18 @@
+<script>
+    import List from 'phosphor-svelte/lib/List'
+</script>
+
+
+<div class="drawer z-50">
+  <input id="my-drawer" type="checkbox" class="drawer-toggle" />
+  <div class="drawer-content">
+    <!-- Page content here -->
+    <label for="my-drawer" class="btn btn-ghost drawer-button"><List size={22}/></label>
+  </div> 
+  <div class="drawer-side">
+    <label for="my-drawer" aria-label="close sidebar" class="drawer-overlay"></label>
+    <ul class="menu p-4 w-96 min-h-full bg-base-200 text-base-content">
+      <li><a class="button btn-ghost bg-black" href="/register">Register</a></li>      
+    </ul>
+  </div>
+</div>
