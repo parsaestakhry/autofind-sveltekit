b5fe018593e042969d628cafb8c6c630627825ad
Parsa Estakhri <parsaestakhry03@gmail.com>
2024-05-24 12:13:54 +0330

    added register board

--- src/routes/register/+page.svelte
+++ src/routes/register/+page.svelte
@@ -1,2 +1,9 @@
 <script>
+	import RegisterBoard from '../../components/RegisterBoard.svelte'
 </script>
+
+<div class="flex ">
+	<div class="artboard artboard-horizontal phone-5 bg-black m-auto">
+		<RegisterBoard/>
+	</div>
+</div>
