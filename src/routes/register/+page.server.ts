900f541a8cdfa07001a3d607dd900aa4ad601bfb
Parsa Estakhri <parsaestakhry03@gmail.com>
2024-05-25 20:06:10 +0330

    accessed the form data

--- src/routes/register/+page.server.ts
+++ src/routes/register/+page.server.ts
@@ -1,5 +1,9 @@
-import type { PageServerLoad } from './$types';
+import type { Actions } from './$types';
 
-export const load = (async () => {
-	return {};
-}) satisfies PageServerLoad;
+export const actions = {
+	default: async (event) => {
+		// TODO log the user in
+		const body = await event.request.formData();
+		console.log(...body)
+	}
+} satisfies Actions;
