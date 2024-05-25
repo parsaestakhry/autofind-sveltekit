2ae7d0a0761fd5044861ad4f99d705f45dc078e3
Parsa Estakhri <parsaestakhry03@gmail.com>
2024-05-25 21:47:51 +0330

    hashed password

--- src/routes/register/+page.server.ts
+++ src/routes/register/+page.server.ts
@@ -1,9 +1,21 @@
+import { RegisterUser } from '$lib/server/RegisterUser';
+import { saltAndHashPassword } from '../../utils/password';
 import type { Actions } from './$types';
 
 export const actions = {
 	default: async (event) => {
 		// TODO log the user in
 		const body = await event.request.formData();
-		console.log(...body)
+		const infoArray = [...body];
+		const email = infoArray[0][1];
+		const username = infoArray[1][1];
+		const firstname = infoArray[2][1];
+		const lastname = infoArray[3][1];
+		let password = infoArray[4][1];
+		password = password.toString();
+		const hash = await saltAndHashPassword(password);
+		
+
+		
 	}
 } satisfies Actions;
