605324203d2bc5a21be16e36de8458e09b74c001
Parsa Estakhri <parsaestakhry03@gmail.com>
2024-05-25 21:48:05 +0330

    made the component a promise

--- src/utils/password.ts
+++ src/utils/password.ts
@@ -1,8 +1,16 @@
-import bcrypt from 'bcrypt'
-export function saltAndHashPassword(password : string) {
+import bcrypt from 'bcrypt';
+
+export function saltAndHashPassword(password: string): Promise<string> {
 	const saltRounds = 10;
-	const myPlaintextPassword = password
-	bcrypt.hash(myPlaintextPassword, saltRounds, function (err : any, hash : string) {
-		return hash
+	const myPlaintextPassword = password;
+
+	return new Promise((resolve, reject) => {
+		bcrypt.hash(myPlaintextPassword, saltRounds, function (err: any, hash: string) {
+			if (err) {
+				reject(err);
+			} else {
+				resolve(hash);
+			}
+		});
 	});
 }
