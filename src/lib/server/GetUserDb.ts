62bb5993f3efe25bfd4f0149570599516083ad8c
Parsa Estakhri <parsaestakhry03@gmail.com>
2024-05-30 18:15:14 +0330

    removed pwhash

--- src/lib/server/GetUserDb.ts
+++ src/lib/server/GetUserDb.ts
@@ -1,8 +1,9 @@
 import { connection } from '$lib/db/mysql';
+import { checkPassword } from '../../utils/CheckPassword';
 
-export async function getUserFromDb(username: string, pwHash: string) {
+export async function getUserFromDb(username: string) {
 	let results = await connection
-		.query(`SELECT * FROM user WHERE username = "${username}" AND password="${pwHash}";`)
+		.query(`SELECT * FROM user WHERE username = "${username}"`)
 		.then(function ([rows, fields]) {
 			return rows;
 		});
