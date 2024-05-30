c3f5beb68f0e823f4e3e098705e1402324841ae4
Parsa Estakhri <parsaestakhry03@gmail.com>
2024-05-30 18:15:28 +0330

    check for user password correctness

--- src/routes/login/+page.server.ts
+++ src/routes/login/+page.server.ts
@@ -2,16 +2,26 @@ import { getUserFromDb } from '$lib/server/GetUserDb';
 import { checkPassword } from '../../utils/CheckPassword';
 import { saltAndHashPassword } from '../../utils/GenPassword';
 import type { PageServerLoad } from './$types';
-
+import jwt from 'jsonwebtoken';
 export const actions = {
-	default: async (event) => {
+	default: async (event: any) => {
 		const body = await event.request.formData();
 		const infoArray = [...body];
 		const username = infoArray[0][1].toString();
 		const password = infoArray[1][1].toString();
 		const pwHash = await saltAndHashPassword(password);
-		const result = await getUserFromDb(username, pwHash);
-		const compareResult = await checkPassword(password, pwHash)
+		const result = await getUserFromDb(username);
+		const userArray = result.body as { password: string }[];
+		if (userArray.length > 0) {
+			const userPassword = userArray[0].password;
+			const compareResult = await checkPassword(password, userPassword);
+			if (compareResult) {
+				console.log('correct password')
+			} else {
+				console.log('incorrect password')
+			}
+			
+		}
 		
-	}	
+	}
 };
