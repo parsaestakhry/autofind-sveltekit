fb997d268c15d4cd242c5cc3f3dbb3e470facfe8
Parsa Estakhri <parsaestakhry03@gmail.com>
2024-05-30 18:32:11 +0330

    added dotenv

--- src/routes/login/+page.server.ts
+++ src/routes/login/+page.server.ts
@@ -3,25 +3,49 @@ import { checkPassword } from '../../utils/CheckPassword';
 import { saltAndHashPassword } from '../../utils/GenPassword';
 import type { PageServerLoad } from './$types';
 import jwt from 'jsonwebtoken';
+import dotenv from 'dotenv';
+
+dotenv.config();
+
 export const actions = {
 	default: async (event: any) => {
-		const body = await event.request.formData();
-		const infoArray = [...body];
-		const username = infoArray[0][1].toString();
-		const password = infoArray[1][1].toString();
-		const pwHash = await saltAndHashPassword(password);
-		const result = await getUserFromDb(username);
-		const userArray = result.body as { password: string }[];
-		if (userArray.length > 0) {
-			const userPassword = userArray[0].password;
-			const compareResult = await checkPassword(password, userPassword);
-			if (compareResult) {
-				console.log('correct password')
-			} else {
-				console.log('incorrect password')
+		try {
+			const body = await event.request.formData();
+			const username = body.get('username')?.toString();
+			const password = body.get('password')?.toString();
+
+			if (!username || !password) {
+				console.log('Username or password missing');
+				return;
 			}
-			
+
+			const result = await getUserFromDb(username);
+			const userArray = result.body as { password: string; username: string }[];
+
+			if (userArray.length > 0) {
+				const userPassword = userArray[0].password;
+				const userName = userArray[0].username;
+				const compareResult = await checkPassword(password, userPassword);
+
+				if (compareResult) {
+					console.log('Correct password');
+					const tokenSecret = process.env.ACCESS_TOKEN_SECRET;
+
+					if (!tokenSecret) {
+						throw new Error('Access token secret not defined');
+					}
+
+					const userObject = { username: userName };
+					const accessToken = jwt.sign(userObject, tokenSecret);
+					console.log(accessToken);
+				} else {
+					console.log('Incorrect password');
+				}
+			} else {
+				console.log('User not found');
+			}
+		} catch (error) {
+			console.error('Internal Server Error', error);
 		}
-		
 	}
 };
