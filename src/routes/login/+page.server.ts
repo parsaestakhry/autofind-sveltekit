959672de0b4d683a91d34a3e9e6ac3de96e2e333
Parsa Estakhri <parsaestakhry03@gmail.com>
2024-05-30 15:45:52 +0330

    console log

--- src/routes/login/+page.server.ts
+++ src/routes/login/+page.server.ts
@@ -1,15 +1,15 @@
 import { getUserFromDb } from '$lib/server/GetUserDb';
-import { saltAndHashPassword } from '../../utils/password';
+import { saltAndHashPassword } from '../../utils/GenPassword';
 import type { PageServerLoad } from './$types';
 
 export const actions = {
 	default: async (event) => {
 		const body = await event.request.formData();
 		const infoArray = [...body];
-		const username = infoArray[0][1].toString()
-		const password = infoArray[1][1].toString()
-		const pwHash = await saltAndHashPassword(password)
-		const result = await getUserFromDb(username, pwHash)
-		console.log(result)
+		const username = infoArray[0][1].toString();
+		const password = infoArray[1][1].toString();
+		const pwHash = await saltAndHashPassword(password);
+		const result = await getUserFromDb(username, pwHash);
+		console.log(pwHash);
 	}
-}
+};
