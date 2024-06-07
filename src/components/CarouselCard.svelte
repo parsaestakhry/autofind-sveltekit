27254acd59419e0fe6f7004197197fdb812044ca
Parsa Estakhri <parsaestakhry03@gmail.com>
2024-06-07 22:02:01 +0330

    added hatchback to carousel

---
+++ src/components/CarouselCard.svelte
@@ -0,0 +1,10 @@
+<script>
+    import hatchback from '../assets/images/hothatch.jpg';
+</script>
+
+<div class="relative w-[300px]  overflow-hidden">
+    <img class="object-right-bottom absolute inset-0 w-full h-full rounded-lg " src={hatchback} alt="hatch" />
+    <div class="absolute inset-0 flex items-center justify-center font-bold ">
+        <span class="text-2xl mb-10 text-black">Hatchbacks</span>
+    </div>
+</div>
