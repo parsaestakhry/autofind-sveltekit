8179746eacbc45124fe6a851e63d15ff8d80e71c
Parsa Estakhri <parsaestakhry03@gmail.com>
2024-06-07 22:02:20 +0330

    implemented Carousel card

---
+++ src/components/Carousel.svelte
@@ -0,0 +1,17 @@
+<script>
+	import CarouselCard from "./CarouselCard.svelte";
+
+</script>
+<div class="carousel carousel-center p-4 space-x-4 bg-ghost rounded-box mx-40">
+	<div class="flex space-x-4">
+		<div class="carousel-item">
+			<CarouselCard/>
+		</div>
+		<div class="carousel-item">
+			<img
+				src="https://img.daisyui.com/images/stock/photo-1601004890684-d8cbf643f5f2.jpg"
+				class="rounded-box"
+			/>
+		</div>
+	</div>
+</div>
