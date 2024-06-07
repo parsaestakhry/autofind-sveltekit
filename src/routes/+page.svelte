05b7dbbc5379287c4d3f5f74b1a7f47e9dfa9249
Parsa Estakhri <parsaestakhry03@gmail.com>
2024-06-07 22:02:09 +0330

    css

--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -1,11 +1,12 @@
 <script>
 	import intropic from '../assets/images/intro-pic.jpg';
+	import Carousel from '../components/Carousel.svelte';
 </script>
 
 <div class="relative w-screen sm:h-64 h-80 overflow-hidden">
 	<img class="object-cover object-center w-full h-full" src={intropic} alt="family-car" />
 	<div class="absolute inset-0 font-bold items-center justify-start ml-5">
-		<h1 class="text-white sm:text-7xl sm:mt-10 text-6xl mt-16">Your Next Car is Here.</h1>
+		<h1 class="text-white sm:text-7xl sm:mt-14 text-6xl mt-16">Your Next Car is Here.</h1>
         <div class="sm:hidden justify-end flex-col items-end mr-20 mt-3 space-y-2 ">
 			<button
 				class="btn  text-slate-50 text-xl bg-orange-500 hover:bg-orange-600 border-none font-bold"
@@ -16,7 +17,7 @@
 				>Sell My Car</button
 			>
 		</div>
-		<div class="sm:flex justify-end flex-col items-end mr-20 -mt-10 space-y-2 hidden">
+		<div class="sm:flex justify-end flex-col items-end mr-20 -mt-10 space-y-3 hidden">
 			<button
 				class="btn w-48 h-18 text-slate-50 text-3xl bg-orange-500 hover:bg-orange-600 border-none font-bold"
 				>Buy a Car</button
@@ -28,3 +29,5 @@
 		</div>
 	</div>
 </div>
+
+<Carousel/>
