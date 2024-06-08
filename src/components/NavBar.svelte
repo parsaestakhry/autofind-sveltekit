f83dce3d8f8e30e96800dfb509ac9554023487f9
Parsa Estakhri <parsaestakhry03@gmail.com>
2024-06-08 12:39:19 +0330

    changed navbar color

--- src/components/NavBar.svelte
+++ src/components/NavBar.svelte
@@ -4,10 +4,10 @@
 	import SignIn from 'phosphor-svelte/lib/SignIn';
 </script>
 
-<div class="navbar bg-ghost rounded-md">
+<div class="navbar bg-orange-600 ">
 	<div class="flex-none"></div>
 	<div class="flex-1">
-		<a href="/" class="btn btn-ghost text-2xl font-sans text-slate-100 ml-10"
+		<a href="/" class="btn btn-ghost text-2xl font-bold text-slate-800 ml-10"
 			>Autofind <CarBattery size={25} class="mt-1" />
 			<div></div>
 		</a>
