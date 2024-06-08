c15277e69a66adf99fff6cfe27ac975fab188a6f
Parsa Estakhri <parsaestakhry03@gmail.com>
2024-06-08 12:34:01 +0330

    css

--- src/components/CarouselCard.svelte
+++ src/components/CarouselCard.svelte
@@ -1,10 +1,11 @@
 <script>
-    import hatchback from '../assets/images/hothatch.jpg';
+	export let name;
+	export let pic;
 </script>
 
-<div class="relative w-[300px]  overflow-hidden">
-    <img class="object-right-bottom absolute inset-0 w-full h-full rounded-lg " src={hatchback} alt="hatch" />
-    <div class="absolute inset-0 flex items-center justify-center font-bold ">
-        <span class="text-2xl mb-10 text-black">Hatchbacks</span>
-    </div>
+<div class="relative w-80">
+	<img class="object-cover inset-0 w-full h-full rounded-lg" src={pic} alt="hatch" />
+	<div class="absolute inset-0 flex items-center justify-center font-bold">
+		<span class="text-3xl mb-20 text-black font-bold">{name}</span>
+	</div>
 </div>
