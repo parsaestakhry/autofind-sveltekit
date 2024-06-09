bc4c8a06a912a41a9c009cbb4abdd006cdf06589
Parsa Estakhri <parsaestakhry03@gmail.com>
2024-06-09 09:33:10 +0330

    rendering cars as cards

--- src/routes/cars/categories/[slug]/+page.svelte
+++ src/routes/cars/categories/[slug]/+page.svelte
@@ -1,28 +1,58 @@
-<script>
-    export let data;
-	import hatchback from '../../../../assets/images/longhatch.jpg'
-	import sedan from '../../../../assets/images/longsedan.jpg'
-	import coupe from '../../../../assets/images/longcoupe.jpg'
-	import truck from '../../../../assets/images/longtruck.jpg'
-	import wagon from '../../../../assets/images/longwagon.jpg'
-	let pic = "";
+<script lang="ts">
+	export let data;
+	import hatchback from '../../../../assets/images/longhatch.jpg';
+	import sedan from '../../../../assets/images/longsedan.jpg';
+	import coupe from '../../../../assets/images/longcoupe.jpg';
+	import truck from '../../../../assets/images/longtruck.jpg';
+	import wagon from '../../../../assets/images/longwagon.jpg';
+	import type { Car } from '$lib/server/GetCars.js';
+	import Card from '../../../../components/Card.svelte';
+	let pic = '';
 	switch (data.slug) {
-		case "hatchback" : pic = hatchback 
-		break
-		case "sedan" : pic = sedan 
-		break
-		case "coupe" : pic = coupe 
-		break
-		case "truck" : pic = truck 
-		break
-		case "wagon" : pic = wagon 
-		break
-	} 
+		case 'hatchback':
+			pic = hatchback;
+			break;
+		case 'sedan':
+			pic = sedan;
+			break;
+		case 'coupe':
+			pic = coupe;
+			break;
+		case 'truck':
+			pic = truck;
+			break;
+		case 'wagon':
+			pic = wagon;
+			break;
+	}
 
+	const cars: Car[] = data.cars;
 </script>
+
 <div class="relative sm:h-64 h-80">
-	<img class="object-cover object-center w-screen h-28  sm:h-72" src={pic} alt="family-car" />
-	<div class="absolute inset-0 font-bold  text-center ">
-		<h1 class="text-white sm:text-7xl  text-5xl mt-10 sm:mt-16">{data.slug.toUpperCase()}S</h1>
+	<img class="object-cover object-center w-screen h-28 sm:h-72" src={pic} alt="family-car" />
+	<div class="absolute inset-0 font-bold text-center">
+		<h1 class="text-white sm:text-7xl text-5xl mt-10 sm:mt-16">{data.slug.toUpperCase()}S</h1>
 	</div>
+</div>
+
+<div class="sm:mt-10">
+	{#if cars && cars.length > 0}
+		<ul class="flex flex-wrap justify-center mt-2">
+			{#each cars as car}
+				<li>
+					<Card
+						model={car.model}
+						make={car.make}
+						milage={car.milage}
+						color={car.Color}
+						fuel_type={car.fuel_type}
+						year_make={car.year_make}
+					/>
+				</li>
+			{/each}
+		</ul>
+	{:else}
+		<p>No cars available.</p>
+	{/if}
 </div>
