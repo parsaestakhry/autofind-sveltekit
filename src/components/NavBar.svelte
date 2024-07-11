f768b40a8a2d22ca9c0ab2ec37e70c885bc58ef4
Parsa Estakhri <parsaestakhry03@gmail.com>
2024-07-11 15:55:41 +0330

    css

--- src/components/NavBar.svelte
+++ src/components/NavBar.svelte
@@ -4,7 +4,7 @@
 	import SignIn from 'phosphor-svelte/lib/SignIn';
 </script>
 
-<div class="navbar bg-orange-600 ">
+<div class="navbar bg-orange-600">
 	<div class="flex-none"></div>
 	<div class="flex-1">
 		<a href="/" class="btn btn-ghost text-2xl font-bold text-slate-800 ml-10"
@@ -12,9 +12,9 @@
 			<div></div>
 		</a>
 	</div>
+	
 	<div class="flex-none">
 		<a class="btn btn-ghost" href="/login"><SignIn weight="bold" color="#f7f2f2" size={27} /></a>
-
 		<Drawer />
 	</div>
 </div>
