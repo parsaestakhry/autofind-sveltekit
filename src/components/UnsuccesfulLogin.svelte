5f2f9dc1f9700d0a2402ac177d45da2456e4c921
Parsa Estakhri <parsaestakhry03@gmail.com>
2024-07-11 15:56:03 +0330

    added timer

--- src/components/UnsuccesfulLogin.svelte
+++ src/components/UnsuccesfulLogin.svelte
@@ -1,5 +1,20 @@
+<script>
+  import { onMount } from 'svelte';
+	let visible = true;
+	onMount(() => {
+		const timer = setTimeout(() => {
+			visible = false;
+		}, 5000);
+		return () => {
+			clearTimeout(timer);
+		};
+	});
+</script>
+
+{#if visible}
 <div class="toast">
   <div class="alert alert-error">
     <span>Login unsuccessful</span>
   </div>
 </div>
+{/if}
