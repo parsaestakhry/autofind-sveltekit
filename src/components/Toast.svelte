65fd8ad56b6fc6563a003b376c7687de2001d6ba
Parsa Estakhri <parsaestakhry03@gmail.com>
2024-07-11 15:55:56 +0330

    added timer for component appearance

--- src/components/Toast.svelte
+++ src/components/Toast.svelte
@@ -1,5 +1,21 @@
+<script>
+	import { onMount } from 'svelte';
+	let visible = true;
+	onMount(() => {
+		const timer = setTimeout(() => {
+			visible = false;
+		}, 5000);
+		return () => {
+			clearTimeout(timer);
+		};
+	});
+</script>
+
+
+{#if visible}
 <div class="toast">
-  <div class="alert  alert-success">
-    <span>Login Successful</span>
-  </div>
+	<div class="alert alert-success">
+		<span>Login Successful</span>
+	</div>
 </div>
+{/if}
