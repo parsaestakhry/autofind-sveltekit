0ceef9d79a4d6147996d4bb8cfce0f5bcbf155d9
Parsa Estakhri <parsaestakhry03@gmail.com>
2024-07-12 23:25:25 +0330

    button

--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -15,12 +15,12 @@
 		<h1 class="text-white sm:text-7xl sm:mt-14 text-6xl mt-16">Your Next Car is Here.</h1>
 		<div class="sm:hidden justify-end flex-col items-end mr-20 mt-3 space-y-2">
 			<button
-				class="btn text-slate-50 text-xl bg-orange-600 hover:bg-orange-600 border-none font-bold"
+				class="btn text-slate-900 text-xl bg-orange-600 hover:bg-orange-600 border-none font-bold"
 			>
 				Buy a Car
 			</button>
 			<button
-				class="btn text-slate-50 text-xl bg-orange-600 hover:bg-orange-600 border-none font-bold"
+				class="btn text-slate-900 text-xl bg-orange-600 hover:bg-orange-600 border-none font-bold"
 			>
 				Sell My Car
 			</button>
