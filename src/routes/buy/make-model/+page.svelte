81798b13f022b59a66576cfc8fb6e3739857867a
Parsa Estakhri <parsaestakhry03@gmail.com>
2024-07-12 23:25:36 +0330

    hero

--- src/routes/buy/make-model/+page.svelte
+++ src/routes/buy/make-model/+page.svelte
@@ -1,3 +1,42 @@
+<script>
+	import nissan from '../../../assets/images/pngimg.com - nissan_PNG52.png';
+</script>
+
 <div>
-    hello make and model
+	<section class="bg-white dark:bg-gray-900">
+		<div class="grid max-w-screen-xl px-4 py-8 mx-auto lg:gap-8 xl:gap-0 lg:py-16 lg:grid-cols-12">
+			<div class="mr-auto place-self-center lg:col-span-7">
+				<h1
+					class="max-w-2xl mb-4 text-4xl font-extrabold tracking-tight leading-none md:text-5xl xl:text-6xl dark:text-white"
+				>
+					Payments tool for software companies
+				</h1>
+				<p
+					class="max-w-2xl mb-6 font-light text-gray-500 lg:mb-8 md:text-lg lg:text-xl dark:text-gray-400"
+				>
+					From checkout to global sales tax compliance, companies around the world use Flowbite to
+					simplify their payment stack.
+				</p>
+				<div class="dropdown">
+					<div tabindex="0" role="button" class="btn m-1">Click</div>
+					<ul
+						
+						class="dropdown-content menu bg-base-100 rounded-box z-[1] w-52 p-2 shadow"
+					>
+						<li><button>Accura</button></li>
+						<li><a>Item 2</a></li>
+					</ul>
+				</div>
+				<a
+					href="#"
+					class="inline-flex items-center justify-center px-5 py-3 text-base font-medium text-center text-gray-900 border border-gray-300 rounded-lg hover:bg-gray-100 focus:ring-4 focus:ring-gray-100 dark:text-white dark:border-gray-700 dark:hover:bg-gray-700 dark:focus:ring-gray-800"
+				>
+					Speak to Sales
+				</a>
+			</div>
+			<div class="hidden lg:mt-0 lg:col-span-5 lg:flex">
+				<img src={nissan} alt="mockup" />
+			</div>
+		</div>
+	</section>
 </div>
