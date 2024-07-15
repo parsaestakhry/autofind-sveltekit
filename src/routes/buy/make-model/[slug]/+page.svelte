92b1dc863539c67f662d92f325bc292e2c3fb97d
Parsa Estakhri <parsaestakhry03@gmail.com>
2024-07-15 12:30:34 +0330

    make page models

--- src/routes/buy/make-model/[slug]/+page.svelte
+++ src/routes/buy/make-model/[slug]/+page.svelte
@@ -1,3 +1,12 @@
+<script>
+	import TableHeader from "../../../../components/TableHeader.svelte";
+    import Hero from '../../../../components/Hero.svelte'
+    export let data;
+    const make = data.make
+</script>
+
+
 <div>
-    hello
+    <Hero header={make} />
 </div>
+
