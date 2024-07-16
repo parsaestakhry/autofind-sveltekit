a970f841339b59f3f049787d87fa1fc3cf2de287
Parsa Estakhri <parsaestakhry03@gmail.com>
2024-07-16 11:13:58 +0330

    added labels and props

---
+++ src/components/LabelCheckBox.svelte
@@ -0,0 +1,12 @@
+<script>
+    export let item
+</script>
+
+
+
+<div class="form-control">
+	<label class="label cursor-pointer">
+		<span class="label-text -my-10 w-16 font-bold text-md">{item}</span>
+		<input type="checkbox" checked={false} class="checkbox ml-10" />
+	</label>
+</div>
