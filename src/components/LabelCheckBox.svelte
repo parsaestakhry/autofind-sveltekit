80637661021da8f991fc2900504a059eecca2e16
Parsa Estakhri <parsaestakhry03@gmail.com>
2024-07-16 18:45:19 +0330

    check function for the labels

--- src/components/LabelCheckBox.svelte
+++ src/components/LabelCheckBox.svelte
@@ -1,12 +1,22 @@
-<script>
-    export let item
+<script lang="ts">
+	export let item : string;
+	let yes = false;
+	import { selectedItems } from '../store/store';
+	import { get } from 'svelte/store';
+	function toggleSelection() {
+		let currentSelection = get(selectedItems);
+
+		if (yes) {
+			selectedItems.set([...currentSelection, item]);
+		} else {
+			selectedItems.set(currentSelection.filter((i) => i !== item));
+		}
+	}
 </script>
-
-
 
 <div class="form-control">
 	<label class="label cursor-pointer">
 		<span class="label-text -my-10 w-16 font-bold text-md">{item}</span>
-		<input type="checkbox" checked={false} class="checkbox ml-10" />
+		<input type="checkbox" bind:checked={yes} class="checkbox ml-8" />
 	</label>
 </div>
