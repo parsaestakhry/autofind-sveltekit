b7629b88d137a11275a19cb0d708fcbdd4da4377
Parsa Estakhri <parsaestakhry03@gmail.com>
2024-07-18 13:52:58 +0330

    added info

--- src/components/Card.svelte
+++ src/components/Card.svelte
@@ -8,7 +8,7 @@
 	export let price;
 	let carImage: string;
 	import defaultJPG from '../assets/images/default.jpg';
-	import { Calendar, CurrencyDollar, GasPump, Palette, Phone, PhoneCall, RoadHorizon } from 'phosphor-svelte';
+	import { Calendar, CurrencyDollar, GasPump, Info, Palette, Phone, PhoneCall, RoadHorizon } from 'phosphor-svelte';
 	// Dynamically import the image based on the make
 	import(`../assets/images/${make}.jpg`)
 		.then((image) => {
@@ -21,12 +21,12 @@
 		});
 </script>
 
-<div class="card sm:w-96 w-81 bg-orange-600 mt-5 mx-5">
+<div class="card sm:w-[28rem] w-81 bg-orange-600 mt-5 mx-5 mb-10 ">
 	<figure><img src={defaultJPG} alt={make} /></figure>
 	<div class="card-body">
 		<h2 class="card-title text-slate-800 text-2xl font-extrabold tracking-wide">{make}</h2>
 		<h3 class=" text-slate-800 text-xl font-bold">{model}</h3>
-		<div class="space-y-2">
+		<div class=" space-y-1  ">
 			<div class="badge bg-slate-800 border-none text-slate-50 text-md badge-lg font-semibold p-4">
 				Milage : {milage} <RoadHorizon class="ml-3" size={20} weight="bold" />
 			</div>
@@ -45,13 +45,18 @@
 				<button
 					class="btn bg-orange-600 border-none text-slate-800 mt-2 h-14 text-lg hover:bg-orange-700"
 				>
-					<CurrencyDollar size={23} weight="bold" />
+					{price}<CurrencyDollar size={23} weight="bold" />
 				</button>
 			</div>
 		{:else}
-			<div class="card-actions justify-start">
+			<div class="card-actions justify-center">
 				<button
-					class="btn bg-orange-600 border-none text-slate-800  h-14 text-lg hover:bg-orange-700 font-bold"
+					class="btn btn-ghost border-none text-slate-800  h-14 text-lg hover:bg-orange-700 font-bold"
+				>
+					Learn More <Info size={23} weight="bold" />
+				</button>
+				<button
+					class="btn btn-ghost border-none text-slate-800  h-14 text-lg hover:bg-orange-700 font-bold"
 				>
 					Contact Dealer <PhoneCall size={23} weight="bold" />
 				</button>
