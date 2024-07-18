fd7ff2e34cf7266630d3780a1fb7887e7507282e
Parsa Estakhri <parsaestakhry03@gmail.com>
2024-07-18 16:22:36 +0330

    implemented year query

--- src/routes/api/make-model/+server.ts
+++ src/routes/api/make-model/+server.ts
@@ -5,23 +5,28 @@ export async function POST(event: any) {
 	const body = await event.request.json();
 	//console.log('request body: ', body);
 	const make = body.makeChoice;
-	const model = body.modelChoice
-	let query = ``
+	const model = body.modelChoice;
+	const year = body.yearChoice;
+	let query = ``;
 	//console.log(choice);
-	const modelQuery = `SELECT * FROM car WHERE make = "${make}" AND model = "${model}"`
+	const yearQuery = `SELECT * FROM car WHERE make = "${make}" AND model = "${model}" AND year_make = ${year}`;
+	const modelQuery = `SELECT * FROM car WHERE make = "${make}" AND model = "${model}"`;
 	const makeQuery = `SELECT * FROM car WHERE make = "${make}" `;
+
 	if (model !== undefined) {
-		query = modelQuery
+		query = modelQuery;
+		if (year !== undefined) {
+			query = yearQuery;
+		}
 	} else {
-		query = makeQuery
+		query = makeQuery;
 	}
 
-	let results: Car[] | null = await connection
-		.query(query)
-		.then(function ([rows, fields]) {
-			//console.log(rows)
-			return rows as Car[];
-		});
+
+	let results: Car[] | null = await connection.query(query).then(function ([rows, fields]) {
+		//console.log(rows)
+		return rows as Car[];
+	});
 
 	return new Response(JSON.stringify(results));
 }
