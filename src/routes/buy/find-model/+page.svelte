f65a92a4514fbfe476052c623a7466e7f54ca007
Parsa Estakhri <parsaestakhry03@gmail.com>
2024-07-19 17:43:40 +0330

    added range query

--- src/routes/buy/find-model/+page.svelte
+++ src/routes/buy/find-model/+page.svelte
@@ -1,54 +1,65 @@
-<script>
-    
+<script lang="ts">
+	import GLS from '../../../assets/images/gls.webp';
+	let minValue: number;
+	let maxValue: number;
 </script>
-
-
-
 
 <div>
 	<section class="bg-white dark:bg-gray-900">
-		<div class="grid max-w-screen-xl px-4 py-8 mx-auto lg:gap-8 xl:gap-0 lg:py-16 lg:grid-cols-12">
+		<div class="grid max-w-screen-xl px-8 py-8 mx-auto lg:gap-8 xl:gap-0 lg:py-16 lg:grid-cols-12">
 			<div class="mr-auto place-self-center lg:col-span-7">
 				<h1
 					class="max-w-2xl mb-4 text-4xl font-extrabold tracking-tight leading-none md:text-5xl xl:text-6xl dark:text-white"
 				>
-					Payments tool for software companies
+					We can find the car you'll love !
 				</h1>
 				<p
 					class="max-w-2xl mb-6 font-light text-gray-500 lg:mb-8 md:text-lg lg:text-xl dark:text-gray-400"
 				>
-					From checkout to global sales tax compliance, companies around the world use Flowbite to
-					simplify their payment stack.
+					From family SUVs and Sedans to Sports Cars, we've got them all.
 				</p>
-				<a
-					href="#"
-					class="inline-flex items-center justify-center px-5 py-3 mr-3 text-base font-medium text-center text-white rounded-lg bg-primary-700 hover:bg-primary-800 focus:ring-4 focus:ring-primary-300 dark:focus:ring-primary-900"
-				>
-					Get started
-					<svg
-						class="w-5 h-5 ml-2 -mr-1"
-						fill="currentColor"
-						viewBox="0 0 20 20"
-						xmlns="http://www.w3.org/2000/svg"
-						><path
-							fill-rule="evenodd"
-							d="M10.293 3.293a1 1 0 011.414 0l6 6a1 1 0 010 1.414l-6 6a1 1 0 01-1.414-1.414L14.586 11H3a1 1 0 110-2h11.586l-4.293-4.293a1 1 0 010-1.414z"
-							clip-rule="evenodd"
-						></path></svg
-					>
-				</a>
-				<a
-					href="#"
-					class="inline-flex items-center justify-center px-5 py-3 text-base font-medium text-center text-gray-900 border border-gray-300 rounded-lg hover:bg-gray-100 focus:ring-4 focus:ring-gray-100 dark:text-white dark:border-gray-700 dark:hover:bg-gray-700 dark:focus:ring-gray-800"
-				>
-					Speak to Sales
-				</a>
+				<div class="sm:flex sm:space-x-20 space-y-10 sm:space-y-0  ">
+					<div>
+						<h1
+							class=" text-4xl font-extrabold tracking-tight leading-none md:text-5xl xl:text-6xl dark:text-white mb-10"
+						>
+							From
+							{#if minValue === null || minValue === undefined}
+								{''}
+							{:else}
+								{minValue}$
+							{/if}
+						</h1>
+						<input
+							type="number"
+							bind:value={minValue}
+							placeholder="Minimum Budget"
+							class=" bg-orange-600 input input-bordered w-48 placeholder:text-slate-800 placeholder:font-bold"
+						/>
+					</div>
+
+					<div>
+						<h1
+							class=" text-4xl font-extrabold tracking-tight leading-none md:text-5xl xl:text-6xl dark:text-white mb-10"
+						>
+							To
+							{#if maxValue === null || maxValue === undefined}
+								{''}
+							{:else}
+								{maxValue}$
+							{/if}
+						</h1>
+						<input
+							type="number"
+							bind:value={maxValue}
+							placeholder="Maximum Budget"
+							class=" bg-orange-600 input input-bordered w-48 placeholder:text-slate-800 placeholder:font-bold "
+						/>
+					</div>
+				</div>
 			</div>
-			<div class="hidden lg:mt-0 lg:col-span-5 lg:flex">
-				<img
-					src="https://flowbite.s3.amazonaws.com/blocks/marketing-ui/hero/phone-mockup.png"
-					alt="mockup"
-				/>
+			<div class="hidden lg:mt-0 lg:col-span-5 lg:flex p-8">
+				<img src={GLS} alt="mockup" />
 			</div>
 		</div>
 	</section>
