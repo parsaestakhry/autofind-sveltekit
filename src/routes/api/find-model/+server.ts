780cb1cd1f1f4c794db6ca81c7746f5db09d4ea5
Parsa Estakhri <parsaestakhry03@gmail.com>
2024-07-20 16:44:20 +0330

    managing multiple queries in the server

--- src/routes/api/find-model/+server.ts
+++ src/routes/api/find-model/+server.ts
@@ -1,19 +1,38 @@
 /** @type {import('./$types').RequestHandler} */
 import { connection } from '$lib/db/mysql';
-import { json } from 'stream/consumers';
 import type { Car } from '$lib/server/GetCars';
-export async function POST(event: any) {
-	const body = await event.request.json();
-    const min = body.min
-    const max = body.max
-	const make = body.makeChoice
-	console.log(max,min,make)
-    let results: Car[] | null = await connection
-			.query(`SELECT * FROM car WHERE price BETWEEN ${min} AND ${max} `)
-			.then(function ([rows, fields]) {
-				//console.log(rows)
-				return rows as Car[];
-			});
 
-    return new Response(JSON.stringify(results))
+export async function POST(event: any) {
+    const body = await event.request.json();
+    const min = body.min;
+    const max = body.max;
+    const types = body.types;
+
+    // Initialize the base query
+    let query: string;
+    let queryParams: (string | number | null)[] = [];
+
+    // Check if min and max are provided
+    if (min !== undefined && max !== undefined) {
+        query = `SELECT * FROM car WHERE price BETWEEN ? AND ?`;
+        queryParams = [min, max];
+    } else {
+        query = `SELECT * FROM car WHERE price IS NULL`;
+    }
+
+    // Check if types are provided
+    if (types && types.length > 0) {
+        const typesList = types.map((type: string) => `'${type}'`).join(', ');
+        query += ` AND type IN (${typesList})`;
+    }
+
+    let results: Car[] | null = await connection
+        .query(query, queryParams)
+        .then(function ([rows, fields]) {
+            return rows as Car[];
+        });
+
+    return new Response(JSON.stringify(results));
 }
+
+
