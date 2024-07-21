6b7f41b6c17ca8eb9e70263be74871390ba377f3
Parsa Estakhri <parsaestakhry03@gmail.com>
2024-07-21 17:48:34 +0330

    css

--- src/routes/sell/+page.svelte
+++ src/routes/sell/+page.svelte
@@ -472,9 +472,7 @@
 	{#if data.username}
 		<section class="bg-white dark:bg-gray-900 min-h-screen">
 			<div class="py-8 px-4 mx-auto max-w-2xl lg:py-16">
-				<h2 class="mb-4 text-5xl font-bold text-gray-900 dark:text-white text-nowrap">
-					Please Add Your Car
-				</h2>
+				
 				<form method="POST" enctype="multipart/form-data">
 					<div class="grid gap-4 sm:grid-cols-2 sm:gap-6">
 						<div class="sm:col-span-2">
@@ -785,9 +783,9 @@
 					</div>
 					<button
 						type="submit"
-						class="inline-flex items-center px-5 py-2.5 mt-4 sm:mt-6 text-2xl font-extrabold text-center text-white bg-primary-700 rounded-lg focus:ring-4 focus:ring-primary-200 dark:focus:ring-primary-900 hover:bg-primary-800"
+						class=" text-slate-800 bg-orange-600 inline-flex items-center px-5 py-2.5 mt-4 sm:mt-6 text-2xl font-extrabold text-center  bg-primary-700 rounded-lg focus:ring-4 focus:ring-primary-200 dark:focus:ring-primary-900 hover:bg-primary-800"
 					>
-						Add product
+						Submit
 					</button>
 				</form>
 			</div>
