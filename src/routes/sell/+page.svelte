915195f27dfba0d18c5feb17df87955d0bcfb5a6
Parsa Estakhri <parsaestakhry03@gmail.com>
2024-07-21 15:59:28 +0330

    added form action

--- src/routes/sell/+page.svelte
+++ src/routes/sell/+page.svelte
@@ -470,20 +470,18 @@
 <section class="bg-white dark:bg-gray-900 min-h-screen">
 	<div class="py-8 px-4 mx-auto max-w-2xl lg:py-16">
 		<h2 class="mb-4 text-xl font-bold text-gray-900 dark:text-white">Add Your Car</h2>
-		<form action="#">
+		<form method="POST" action="#">
 			<div class="grid gap-4 sm:grid-cols-2 sm:gap-6">
 				<div class="sm:col-span-2">
-					<label
-						for="make"
-						class="block mb-2 text-3xl text-gray-900 dark:text-white font-extrabold"
-						>Manufacturer 
+					<label for="make" class="block mb-2 text-3xl text-gray-900 dark:text-white font-extrabold"
+						>Manufacturer
 					</label>
 					<select
 						id="make"
 						class="bg-gray-50 border-none border-none-gray-300 text-gray-900 text-sm rounded-lg focus:ring-primary-500 focus:border-none-primary-500 block w-full p-2.5 dark:bg-gray-700 dark:border-none-gray-600 dark:placeholder-gray-400 dark:text-white dark:focus:ring-primary-500 dark:focus:border-none-primary-500"
 					>
 						<option selected={false}>Select category</option>
-						{#each makeList as make }
+						{#each makeList as make}
 							<option>{make.name}</option>
 						{/each}
 					</select>
@@ -533,9 +531,7 @@
 					/>
 				</div>
 				<div>
-					<label
-						for="type"
-						class="block font-extrabold mb-2 text-2xl text-gray-900 dark:text-white"
+					<label for="type" class="block font-extrabold mb-2 text-2xl text-gray-900 dark:text-white"
 						>Chasis Type</label
 					>
 					<select
@@ -551,9 +547,7 @@
 					</select>
 				</div>
 				<div>
-					<label
-						for="fuel"
-						class="block font-extrabold mb-2 text-2xl text-gray-900 dark:text-white"
+					<label for="fuel" class="block font-extrabold mb-2 text-2xl text-gray-900 dark:text-white"
 						>Fuel Type</label
 					>
 					<select
@@ -635,7 +629,7 @@
 					</select>
 					<label
 						for="description"
-						class="block mb-2 text-sm font-medium text-gray-900 dark:text-white mt-8"
+						class="block mb-2 text-2xl  font-extrabold text-gray-900 dark:text-white mt-8"
 						>Description</label
 					>
 					<textarea
@@ -644,6 +638,38 @@
 						class="block p-2.5 w-full text-sm text-gray-900 bg-gray-50 rounded-lg border-none border-none-gray-300 focus:ring-primary-500 focus:border-none-primary-500 dark:bg-gray-700 dark:border-none-gray-600 dark:placeholder-gray-400 dark:text-white dark:focus:ring-primary-500 dark:focus:border-none-primary-500"
 						placeholder="Your description here"
 					></textarea>
+
+					<div class="flex items-center justify-center w-full mt-10">
+						<label
+							for="dropzone-file"
+							class="flex flex-col items-center justify-center w-full h-64 border-2 border-gray-300 border-dashed rounded-lg cursor-pointer bg-gray-50 dark:hover:bg-gray-800 dark:bg-gray-700 hover:bg-gray-100 dark:border-gray-600 dark:hover:border-gray-500 dark:hover:bg-gray-600"
+						>
+							<div class="flex flex-col items-center justify-center pt-5 pb-6">
+								<svg
+									class="w-8 h-8 mb-4 text-gray-500 dark:text-gray-400"
+									aria-hidden="true"
+									xmlns="http://www.w3.org/2000/svg"
+									fill="none"
+									viewBox="0 0 20 16"
+								>
+									<path
+										stroke="currentColor"
+										stroke-linecap="round"
+										stroke-linejoin="round"
+										stroke-width="2"
+										d="M13 13h3a3 3 0 0 0 0-6h-.025A5.56 5.56 0 0 0 16 6.5 5.5 5.5 0 0 0 5.207 5.021C5.137 5.017 5.071 5 5 5a4 4 0 0 0 0 8h2.167M10 15V6m0 0L8 8m2-2 2 2"
+									/>
+								</svg>
+								<p class="mb-2 text-sm text-gray-500 dark:text-gray-400">
+									<span class="font-semibold">Click to upload</span> or drag and drop
+								</p>
+								<p class="text-xs text-gray-500 dark:text-gray-400">
+									SVG, PNG, JPG or GIF (MAX. 800x400px)
+								</p>
+							</div>
+							<input id="dropzone-file" type="file" class="hidden" />
+						</label>
+					</div>
 				</div>
 			</div>
 			<button
