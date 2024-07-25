ee92b624fc72a98fa39d47a7f4f7639fc62a9f41
Parsa Estakhri <parsaestakhry03@gmail.com>
2024-07-25 19:12:20 +0330

    update vite config to allow uploading file system

--- vite.config.ts
+++ vite.config.ts
@@ -2,5 +2,10 @@ import { sveltekit } from '@sveltejs/kit/vite';
 import { defineConfig } from 'vite';
 import { enhancedImages } from '@sveltejs/enhanced-img';
 export default defineConfig({
-	plugins: [sveltekit(), enhancedImages()]
+	plugins: [sveltekit(), enhancedImages()],
+	server: {
+		fs: {
+			allow: ['uploads']
+		}
+	}
 });
