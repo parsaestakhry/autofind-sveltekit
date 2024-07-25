7ebd2186bdd8bed4f7958ada01e526148311be17
Parsa Estakhri <parsaestakhry03@gmail.com>
2024-07-25 19:12:44 +0330

    css on card, alowing to put car images

--- src/components/Card.svelte
+++ src/components/Card.svelte
@@ -1,62 +1,89 @@
 <script lang="ts">
-	export let model;
-	export let make;
-	export let milage;
-	export let color;
-	export let fuel_type;
-	export let year_make;
-	export let price;
-	export let type;
-	export let transmission
-	let carImage: string;
-	import defaultJPG from '../assets/images/default.jpg';
-	import { Calendar, Car, CurrencyDollar, GasPump, Gear, Info, Palette, Phone, PhoneCall, RoadHorizon } from 'phosphor-svelte';
-	// Dynamically import the image based on the make
-	// import(`../assets/images/${make}.jpg`)
-	// 	.then((image) => {
-	// 		carImage = image.default;
-	// 	})
-	// 	.catch((error) => {
-	// 		console.error(`Failed to load image for make '${make}':`, error);
-	// 		// Handle error, for example, by setting a default image
-	// 		carImage = defaultJPG;
-	// 	});
+	export let model: string;
+	export let make: string;
+	export let milage: number;
+	export let color: string;
+	export let fuel_type: string;
+	export let year_make: number;
+	export let price: number | null;
+	export let type: string;
+	export let transmission: string;
+	export let username: string | null;
+	export let registration: string;
+	import {
+		Calendar,
+		Car,
+		CurrencyDollar,
+		GasPump,
+		Gear,
+		Info,
+		Palette,
+		PhoneCall,
+		RoadHorizon
+	} from 'phosphor-svelte';
+
+	let carImages = {
+		front: '',
+		rear: '',
+		driver: '',
+		passenger: ''
+	};
+
+	const loadCarImages = () => {
+		// Assuming your files are stored with names like "front-image.jpg" in "uploads/username/registration"
+		const basePath = `../../uploads/${username}/${registration}`;
+		carImages.front = `${basePath}/front.jpg`;
+		carImages.rear = `${basePath}/${Date.now()}-rear.jpg`;
+		carImages.driver = `${basePath}/${Date.now()}-driver.jpg`;
+		carImages.passenger = `${basePath}/${Date.now()}-passenger.jpg`;
+	};
+
+	loadCarImages();
 </script>
 
-<div class="card sm:w-[27rem] w-81 bg-orange-600 mt-5 mx-5 mb-10 ">
-	<figure><img src={defaultJPG} alt={make} /></figure>
+<div class="w-81 card mx-5 mb-10 mt-5 bg-orange-600 sm:w-[27rem]">
+	<figure>
+		<img src={carImages.front} alt="Front side of the car" class="h-48 w-full object-cover" />
+	</figure>
+
 	<div class="card-body">
-		<h2 class="card-title text-slate-800 text-2xl font-extrabold tracking-wide">{make}</h2>
-		<h3 class=" text-slate-800 text-xl font-bold">{model}</h3>
-		<div class=" space-y-1  ">
-			<div class="badge bg-slate-800 border-none text-slate-50 text-md badge-lg font-semibold p-4">
-				Milage : {milage} <RoadHorizon class="ml-3" size={20} weight="bold" />
+		<h2 class="card-title text-2xl font-extrabold tracking-wide text-slate-800">{make}</h2>
+		<h3 class="text-xl font-bold text-slate-800">{model}</h3>
+		<div class="space-y-1">
+			<div class="text-md badge badge-lg border-none bg-slate-800 p-4 font-semibold text-slate-50">
+				Milage: {milage}
+				<RoadHorizon class="ml-3" size={20} weight="bold" />
 			</div>
-			<div class="badge bg-slate-800 border-none text-slate-50 text-md badge-lg font-semibold p-4">
-				Year : {year_make} <Calendar class="ml-3" size={20} weight="bold" />
+			<div class="text-md badge badge-lg border-none bg-slate-800 p-4 font-semibold text-slate-50">
+				Year: {year_make}
+				<Calendar class="ml-3" size={20} weight="bold" />
 			</div>
-			<div class="badge bg-slate-800 border-none text-slate-50 text-md badge-lg font-semibold p-4">
-				Color : {color} <Palette class="ml-2" size={20} weight="bold" />
+			<div class="text-md badge badge-lg border-none bg-slate-800 p-4 font-semibold text-slate-50">
+				Color: {color}
+				<Palette class="ml-2" size={20} weight="bold" />
 			</div>
-			<div class="badge bg-slate-800 border-none text-slate-50 text-md badge-lg font-semibold p-4">
-				Fuel : {fuel_type} <GasPump class="ml-2" size={20} weight="bold" />
+			<div class="text-md badge badge-lg border-none bg-slate-800 p-4 font-semibold text-slate-50">
+				Fuel: {fuel_type}
+				<GasPump class="ml-2" size={20} weight="bold" />
 			</div>
-			<div class="badge bg-slate-800 border-none text-slate-50 text-md badge-lg font-semibold p-4">
-				Type : {type} <Car class="ml-2" size={20} weight="bold" />
+			<div class="text-md badge badge-lg border-none bg-slate-800 p-4 font-semibold text-slate-50">
+				Type: {type}
+				<Car class="ml-2" size={20} weight="bold" />
 			</div>
-			<div class="badge bg-slate-800 border-none text-slate-50 text-md badge-lg font-semibold p-4">
-				Transmission : {transmission} <Gear class="ml-2" size={20} weight="bold" />
+			<div class="text-md badge badge-lg border-none bg-slate-800 p-4 font-semibold text-slate-50">
+				Transmission: {transmission}
+				<Gear class="ml-2" size={20} weight="bold" />
 			</div>
 		</div>
 		{#if price}
 			<div class="card-actions justify-center">
 				<button
-					class="btn btn-ghost bg-orange-600 border-none text-slate-800 mt-2 h-14 text-lg hover:bg-orange-700 font-bold"
+					class="btn btn-ghost mt-2 h-14 border-none bg-orange-600 text-lg font-bold text-slate-800 hover:bg-orange-700"
 				>
 					{price}<CurrencyDollar size={23} weight="bold" />
 				</button>
 				<button
-					class="btn btn-ghost border-none text-slate-800 mt-2  h-14 text-lg hover:bg-orange-700 font-bold"
+					class="btn btn-ghost mt-2 h-14 border-none text-lg font-bold text-slate-800 hover:bg-orange-700"
 				>
 					Learn More <Info size={23} weight="bold" />
 				</button>
@@ -64,12 +91,12 @@
 		{:else}
 			<div class="card-actions justify-center">
 				<button
-					class="btn btn-ghost border-none text-slate-800  h-14 text-lg hover:bg-orange-700 font-bold"
+					class="btn btn-ghost h-14 border-none text-lg font-bold text-slate-800 hover:bg-orange-700"
 				>
 					Learn More <Info size={23} weight="bold" />
 				</button>
 				<button
-					class="btn btn-ghost border-none text-slate-800  h-14 text-lg hover:bg-orange-700 font-bold"
+					class="btn btn-ghost h-14 border-none text-lg font-bold text-slate-800 hover:bg-orange-700"
 				>
 					Contact Dealer <PhoneCall size={23} weight="bold" />
 				</button>
