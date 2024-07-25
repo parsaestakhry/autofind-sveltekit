2a964afeb17616d1f2ac89d52d2a1eb6a0cd9a7a
Parsa Estakhri <parsaestakhry03@gmail.com>
2024-07-25 19:13:01 +0330

    added car registration type as a string

--- src/lib/server/GetCars.ts
+++ src/lib/server/GetCars.ts
@@ -12,6 +12,7 @@ export type Car = {
 	engine_model: string;
 	gearbox: string;
 	price : number
+	registration : string
 };
 export const GetCars = async () => {
 	let results: Car[] = await connection.query('SELECT * FROM car LIMIT 10').then(function ([rows, fields]) {
