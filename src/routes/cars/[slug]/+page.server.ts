676f88904edbf34c82bd8c4ee4b6e25e056fb692
Parsa Estakhri <parsaestakhry03@gmail.com>
2024-07-29 13:01:09 +0330

    fetching cars by registration

---
+++ src/routes/cars/[slug]/+page.server.ts
@@ -0,0 +1,21 @@
+import { connection } from '$lib/db/mysql.js';
+import type { Car } from '$lib/server/GetCars.js';
+export const load = async ({ params }) => {
+	//console.log(params.slug)
+
+	function capitalizeFirstLetter(string: string) {
+		return string.charAt(0).toUpperCase() + string.slice(1);
+	}
+
+	let newParam = capitalizeFirstLetter(params.slug);
+
+	let results = await connection
+		.query(`SELECT * FROM car WHERE type = "${newParam}"`)
+		.then(function ([rows, fields]) {
+			return rows as Car[];
+		});
+	return {
+		slug: params.slug,
+		cars: results
+	};
+};
