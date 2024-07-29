a2d34bc2e85c6df6bbf5e5b7b681a85140272eb6
Parsa Estakhri <parsaestakhry03@gmail.com>
2024-07-29 10:19:47 +0330

    suv implementation to the carousel

--- src/components/Carousel.svelte
+++ src/components/Carousel.svelte
@@ -5,15 +5,19 @@
 	import sedan from '../assets/images/sedan.jpg';
 	import wagon from '../assets/images/wagon.jpg';
 	import truck from '../assets/images/truck.jpg';
+	import suv from '../assets/images/suv.jpg'
 </script>
 
-<div class="carousel carousel-center p-7 space-x-4 bg-ghost rounded-box flex sm:mx-20 mb-10">
+<div class="carousel carousel-center p-7 space-x-4 bg-ghost rounded-box flex sm:mx-20 mb-10  ">
 	<div class="flex space-x-4">
 		<a href="/cars/categories/hatchback" class="carousel-item">
 			<CarouselCard name={'Hatchback'} pic={hatchback} />
 		</a>
 		<a href="/cars/categories/sedan"  class="carousel-item">
 			<CarouselCard name={'Sedan'} pic={sedan} />
+		</a>
+		<a href="/cars/categories/suv" class="carousel-item">
+			<CarouselCard name={'SUV'} pic={suv} />
 		</a>
 		<a href="/cars/categories/coupe" class="carousel-item">
 			<CarouselCard name={'Coupe'} pic={coupe} />
@@ -25,5 +29,6 @@
 		<a href="/cars/categories/truck" class="carousel-item">
 			<CarouselCard name={'Truck'} pic={truck} />
 		</a>
+		
 	</div>
 </div>
