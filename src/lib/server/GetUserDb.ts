242437da1337147fc60f7d2f4d4e65690ca60b84
Parsa Estakhri <parsaestakhry03@gmail.com>
2024-08-01 18:26:42 +0330

    returning user id from the database

--- src/lib/server/GetUserDb.ts
+++ src/lib/server/GetUserDb.ts
@@ -3,9 +3,9 @@ import { checkPassword } from '../../utils/CheckPassword';
 
 export async function getUserFromDb(username: string) {
 	let results = await connection
-		.query(`SELECT * FROM user WHERE username = "${username}"`)
+		.query(`SELECT id FROM user WHERE username = "${username}"`)
 		.then(function ([rows, fields]) {
-			return rows;
+			return rows
 		});
 
 	return {
