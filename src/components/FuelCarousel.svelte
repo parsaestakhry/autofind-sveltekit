66e705e07d329cb5751eea510d94635987219840
Parsa Estakhri <parsaestakhry03@gmail.com>
2024-08-04 16:13:32 +0330

    routing carousel items

--- src/components/FuelCarousel.svelte
+++ src/components/FuelCarousel.svelte
@@ -8,13 +8,13 @@
 
 <div class="carousel carousel-center p-10  bg-ghost rounded-box flex mx-auto ">
 	<div class="flex space-x-3">
-		<a href="/cars/categories/hatchback" class="carousel-item">
+		<a href="/cars/fuel-types/Electric" class="carousel-item">
 			<CarouselCard name={'Electric'} pic={tesla} />
 		</a>
-		<a href="/cars/categories/sedan"  class="carousel-item">
+		<a href="/cars/fuel-types/Diesel"  class="carousel-item">
 			<CarouselCard name={'Diesel'} pic={diesel} />
 		</a>
-		<a href="/cars/categories/suv" class="carousel-item">
+		<a href="/cars/fuel-types/Gas" class="carousel-item">
 			<CarouselCard name={'Gas'} pic={petrol} />
 		</a>
 		
