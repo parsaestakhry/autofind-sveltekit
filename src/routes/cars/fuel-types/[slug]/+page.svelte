78a5fdcc1afb02bdcd6a2c107906668a8c716083
Parsa Estakhri <parsaestakhry03@gmail.com>
2024-08-05 00:32:03 +0330

    added gas pump

--- src/routes/cars/fuel-types/[slug]/+page.svelte
+++ src/routes/cars/fuel-types/[slug]/+page.svelte
@@ -1,33 +1,42 @@
 <!-- CarPage.svelte -->
 <script lang="ts">
 	import type { Car } from '$lib/server/GetCars';
-	import Card from '../../../../components/Card.svelte'
+	import { GasPump } from 'phosphor-svelte';
+	import Card from '../../../../components/Card.svelte';
 	import type { PageData } from './$types';
 	export let data;
-	
+
 	const carArray: Car[] | undefined = data.cars;
 </script>
 
-{#if carArray && carArray.length > 0}
-	<ul class="flex flex-wrap justify-center mt-2">
-		{#each carArray as car}
-			<li>
-				<Card
-					model={car.model}
-					make={car.make}
-					milage={car.milage}
-					color={car.Color}
-					fuel_type={car.fuel_type}
-					year_make={car.year_make}
-					price={car.price}
-                    type={car.type}
-					transmission={car.gearbox}
-					registration = {car.registration}
-				/>
-			</li>
-		{/each}
-	</ul>
-{:else}
-	<p>No cars available.</p>
-{/if}
-
+<div class="bg-gray-900">
+	<div class="relative sm:h-96">
+		<div class="absolute inset-0 mt-10 text-center font-bold">
+			<h1 class="mt-5 text-5xl font-extrabold text-white sm:text-7xl flex justify-center items-end ">
+				Fueled by {data.slug} <GasPump size={60} class='ml-2' weight='bold' />
+			</h1>
+		</div>
+	</div>
+	{#if carArray && carArray.length > 0}
+		<ul class=" flex flex-wrap justify-center p-5">
+			{#each carArray as car}
+				<li>
+					<Card
+						model={car.model}
+						make={car.make}
+						milage={car.milage}
+						color={car.Color}
+						fuel_type={car.fuel_type}
+						year_make={car.year_make}
+						price={car.price}
+						type={car.type}
+						transmission={car.gearbox}
+						registration={car.registration}
+					/>
+				</li>
+			{/each}
+		</ul>
+	{:else}
+		<p>No cars available.</p>
+	{/if}
+</div>
