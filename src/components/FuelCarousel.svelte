0ec755d128914e4c87138e91306e565b8c7bb79f
Parsa Estakhri <parsaestakhry03@gmail.com>
2024-08-04 16:02:34 +0330

    added fuel carousel

---
+++ src/components/FuelCarousel.svelte
@@ -0,0 +1,22 @@
+<script>
+	import tesla from '../assets/images/tesla.jpg'
+    import diesel from '../assets/images/diesel.jpg'
+    import petrol from  '../assets/images/petrol.jpg'
+    import CarouselCard from './CarouselCard.svelte';
+
+</script>
+
+<div class="carousel carousel-center p-10  bg-ghost rounded-box flex mx-auto ">
+	<div class="flex space-x-3">
+		<a href="/cars/categories/hatchback" class="carousel-item">
+			<CarouselCard name={'Electric'} pic={tesla} />
+		</a>
+		<a href="/cars/categories/sedan"  class="carousel-item">
+			<CarouselCard name={'Diesel'} pic={diesel} />
+		</a>
+		<a href="/cars/categories/suv" class="carousel-item">
+			<CarouselCard name={'Gas'} pic={petrol} />
+		</a>
+		
+	</div>
+</div>
