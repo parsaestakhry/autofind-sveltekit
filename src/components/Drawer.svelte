7c814fb42001556ba57a609ef86349f3a34923c0
Parsa Estakhri <parsaestakhry03@gmail.com>
2024-08-05 15:02:54 +0330

    added Buy and Sell

--- src/components/Drawer.svelte
+++ src/components/Drawer.svelte
@@ -1,19 +1,76 @@
-<script>
+<script lang="ts">
 	import List from 'phosphor-svelte/lib/List';
+	export let username;
 </script>
 
-<div class="drawer drawer-end  z-50 sm:hidden" >
+<div class="drawer drawer-end z-50 sm:hidden">
 	<input id="my-drawer-2" type="checkbox" class="drawer-toggle" />
 	<div class="drawer-content flex flex-col items-center justify-center">
 		<!-- Page content here -->
-		<label for="my-drawer-2" class="btn btn-ghost drawer-button "><List color="" size={28}/></label>
+		<label for="my-drawer-2" class="btn btn-ghost drawer-button"><List color="" size={28} /></label>
 	</div>
 	<div class="drawer-side">
 		<label for="my-drawer-2" aria-label="close sidebar" class="drawer-overlay"></label>
-		<ul class="menu p-4 w-80 min-h-full bg-base-200 text-base-content space-y-1">
+		<ul class="menu min-h-full w-80 space-y-2 bg-gray-900 p-4 text-base-content">
 			<!-- Sidebar content here -->
-			<li><a class="button btn-ghost bg-black" href="/register">Register</a></li>
-			<li><a class="button btn-ghost bg-black" href="/login">Login</a></li>
+			<li>
+				<a
+					class="button btn-ghost bg-orange-600 text-xl font-extrabold text-slate-800 hover:bg-orange-700"
+					href="/register">Register</a
+				>
+			</li>
+			<li>
+				<a
+					class="button btn-ghost bg-orange-600 text-xl font-extrabold text-slate-800 hover:bg-orange-700"
+					href="/login">Login</a
+				>
+			</li>
+			<li>
+				<a
+					class="button btn-ghost bg-orange-600 text-xl font-extrabold text-slate-800 hover:bg-orange-700"
+					href="/user/{username}">Account</a
+				>
+			</li>
+			<li>
+				<a
+					data-sveltekit-reload
+					href="/cars/usage/new"
+					class="button btn-ghost bg-orange-600 text-xl font-extrabold text-slate-800 hover:bg-orange-700"
+					>New
+				</a>
+			</li>
+			<li>
+				<a
+					data-sveltekit-reload
+					href="/cars/usage/used"
+					class="button btn-ghost bg-orange-600 text-xl font-extrabold text-slate-800 hover:bg-orange-700"
+					>Used
+				</a>
+			</li>
+			<li>
+				<a
+					data-sveltekit-reload
+					href="/cars/fuel-types/electric"
+					class="button btn-ghost bg-orange-600 text-xl font-extrabold text-slate-800 hover:bg-orange-700"
+					>Electric
+				</a>
+			</li>
+			<li>
+				<a
+					data-sveltekit-reload
+					href="/buy"
+					class="button btn-ghost bg-orange-600 text-xl font-extrabold text-slate-800 hover:bg-orange-700"
+					>Buy
+				</a>
+			</li>
+			<li>
+				<a
+					data-sveltekit-reload
+					href="/sell"
+					class="button btn-ghost bg-orange-600 text-xl font-extrabold text-slate-800 hover:bg-orange-700"
+					>Sell
+				</a>
+			</li>
 		</ul>
 	</div>
 </div>
