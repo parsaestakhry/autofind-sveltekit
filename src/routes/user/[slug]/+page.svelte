85b4932ba096ca58cf5cf8c186e563b3f8f1f64b
Parsa Estakhri <parsaestakhry03@gmail.com>
2024-08-08 15:14:48 +0330

    added card for account page

--- src/routes/user/[slug]/+page.svelte
+++ src/routes/user/[slug]/+page.svelte
@@ -2,6 +2,7 @@
 	import AccountCard from "../../../components/AccountCard.svelte";
 	import panamera from '../../../assets/images/panamera.jpg'
 	import rs from '../../../assets/images/rs.jpg'
+	import gt3 from '../../../assets/images/gt3.jpg'
 	export let data;
 </script>
 
@@ -12,5 +13,6 @@
 	<div class="flex justify-center mt-20 sm:space-x-10 flex-wrap  py-10 " >
 		<AccountCard image={panamera} headText={"Add your own car"} paragraph={'so it can join our variety of cars'} button={'Add Now'} link={'/sell'} />
 		<AccountCard image={rs} headText={"Find your next car"} paragraph={'so it can join your garage'} button={'Buy Now'} link={'/buy'} />
+		<AccountCard image={gt3} headText={"Check out your cars"} paragraph={"see how well they're doing "} button={'Check Out!'} link={`/user/${data.username}/cars`} />
 	</div>
 </div>
