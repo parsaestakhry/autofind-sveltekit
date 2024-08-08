7f882fa9baa2c38ad1fba08e0b26120a2708bdee
Parsa Estakhri <parsaestakhry03@gmail.com>
2024-08-08 17:36:51 +0330

    implemented bid card

---
+++ src/components/BidCard.svelte
@@ -0,0 +1,11 @@
+<script>
+    import { Money } from "phosphor-svelte";
+    export let userBid;
+    export let amount;
+</script>
+
+
+<div role="alert" class="alert bg-inherit text-slate-50 border-4 border-orange-600 font-extrabold text-xl mt-2 ">
+    <Money size={30} weight='bold' />
+  <span>Bid by : {userBid} for Amount of  : {amount}$ </span>
+</div>
