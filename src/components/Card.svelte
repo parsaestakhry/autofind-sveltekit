a19593d67fdb1b899e43579a125204665b56e30e
Parsa Estakhri <parsaestakhry03@gmail.com>
2024-08-10 11:12:21 +0330

    image import for card

--- src/components/Card.svelte
+++ src/components/Card.svelte
@@ -9,7 +9,9 @@
 	export let type: string;
 	export let transmission: string;
 	export let registration: string;
-	export let usage : string;
+	export let usage: string;
+	let image: any;
+	import { onMount } from 'svelte';
 	import {
 		Calendar,
 		Car,
@@ -20,34 +22,22 @@
 		Palette,
 		PhoneCall,
 		RoadHorizon,
-
 		Speedometer
-
 	} from 'phosphor-svelte';
+	
+	const sleep = (ms: number | undefined) => new Promise(f => setTimeout(f, ms));
+	
+	onMount(async () => {
+		await sleep(500); // simulate network delay
+		image = (await import(`../../uploads/${registration}/front.jpg`)).default;
+	});
 
-	let carImages = {
-		front: '',
-		rear: '',
-		driver: '',
-		passenger: ''
-	};
 
-	const loadCarImages = () => {
-		// Assuming your files are stored with names like "front-image.jpg" in "uploads/username/registration"
-		const basePath = `../../uploads/${registration}`;
-		carImages.front = `${basePath}/front.jpg`;
-		carImages.rear = `${basePath}/${Date.now()}-rear.jpg`;
-		carImages.driver = `${basePath}/${Date.now()}-driver.jpg`;
-		carImages.passenger = `${basePath}/${Date.now()}-passenger.jpg`;
-	};
-
-	loadCarImages();
 </script>
 
-<div class="w-81 card mx-5 mb-10 sm:mt-5  bg-orange-600 sm:w-[29rem]">
+<div class="w-81 card mx-5 mb-10 bg-orange-600 sm:mt-5 sm:w-[29rem]">
 	<figure>
-		<img src={carImages.front} alt="Front side of the car" class="h-48 w-full object-cover" /> 
-		
+		<img src={image} alt="Front side of the car" class="h-48 w-full object-cover" />
 	</figure>
 
 	<div class="card-body">
@@ -90,7 +80,7 @@
 				>
 					{price}<CurrencyDollar size={23} weight="bold" />
 				</button>
-				<a 
+				<a
 					href="/cars/{registration}"
 					class="btn btn-ghost mt-2 h-14 border-none text-lg font-bold text-slate-800 hover:bg-orange-700"
 				>
