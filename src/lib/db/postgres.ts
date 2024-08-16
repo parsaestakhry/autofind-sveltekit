e65a54e010b73ca8459e38d3cecdfb9cc6027977
Parsa Estakhri <parsaestakhry03@gmail.com>
2024-08-16 11:33:08 +0330

    environment variables for the postgres

--- src/lib/db/postgres.ts
+++ src/lib/db/postgres.ts
@@ -1,10 +1,14 @@
 import pg from 'pg';
+import { POSTGRES_USER } from '$env/static/private'
+import { POSTGRES_PASSWORD } from '$env/static/private';
+import { POSTGRES_HOST } from '$env/static/private';
+import { POSTGRES_DATABASE } from '$env/static/private';
 const { Client } = pg;
 export const connection = new Client({
-	user: 'default',
-	password: 'I0e8MbAqhtQH',
-	host: 'ep-late-scene-a4jiq0fh-pooler.us-east-1.aws.neon.tech',
-	database: 'verceldb',
+	user: POSTGRES_USER,
+	password: POSTGRES_PASSWORD,
+	host: POSTGRES_HOST,
+	database: POSTGRES_DATABASE,
 	ssl:true
 });
 await connection.connect();
