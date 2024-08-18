4c9360e1bc71b1db81f8f71e627cfb268817164a
Parsa Estakhri <parsaestakhry03@gmail.com>
2024-08-18 23:05:08 +0330

    car make upper case

--- src/routes/cars/makes/[slug]/+page.server.ts
+++ src/routes/cars/makes/[slug]/+page.server.ts
@@ -7,12 +7,12 @@ export const load = async ({ params }: { params: { slug: string } }) => {
 	// Use parameterized query for security
 	const queryResult = await connection.query(
 		`SELECT * FROM car WHERE make = $1`,
-		[newParam] // Pass the parameter as an array
+		[newParam.toUpperCase()] // Pass the parameter as an array
 	);
 
 	// Access rows from the query result
 	const results: Car[] = queryResult.rows as Car[];
-
+	console.log(newParam)
 	return {
 		slug: params.slug,
 		cars: results
