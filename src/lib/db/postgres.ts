be6e40b433b5c8c67e74c10e7392e61903a27666
Parsa Estakhri <parsaestakhry03@gmail.com>
2024-08-29 11:25:56 +0330

    changed client to pool

--- src/lib/db/postgres.ts
+++ src/lib/db/postgres.ts
@@ -1,14 +1,11 @@
 import pg from 'pg';
 import { POSTGRES_USER } from '$env/static/private';
 import { POSTGRES_PASSWORD } from '$env/static/private';
-import { POSTGRES_HOST } from '$env/static/private';
+import { POSTGRES_URL } from '$env/static/private';
 import { POSTGRES_DATABASE } from '$env/static/private';
-const { Client } = pg;
-export const connection = new Client({
-	user: POSTGRES_USER,
-	password: POSTGRES_PASSWORD,
-	host: POSTGRES_HOST,
-	database: POSTGRES_DATABASE,
-	ssl: true
+const { Pool } = pg;
+
+export const connection = new Pool({
+	connectionString: POSTGRES_URL
 });
-await connection.connect();
+connection.connect();
