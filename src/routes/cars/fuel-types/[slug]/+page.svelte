2dff4fb9b5a1ecc90f14dc5addf7d62d79cfce3a
Parsa Estakhri <parsaestakhry03@gmail.com>
2024-08-29 10:47:53 +0330

    styling on the fuel page

--- src/routes/cars/fuel-types/[slug]/+page.svelte
+++ src/routes/cars/fuel-types/[slug]/+page.svelte
@@ -9,36 +9,40 @@
 	const carArray: Car[] | undefined = data.cars;
 </script>
 
-<div class="bg-gray-900">
-	<div class="relative sm:h-96">
-		<div class="absolute inset-0 mt-10 text-center font-bold">
-			<h1 class="mt-5 flex items-end justify-center text-5xl font-extrabold text-white sm:text-7xl">
-				Fueled by {data.slug}
-				<GasPump size={60} class="ml-2" weight="bold" />
-			</h1>
+<div class="">
+	<div class="min-h-screen bg-gray-900">
+		<div class="relative sm:h-96">
+			<div class="  text-center font-bold flex justify-center ">
+				<h1
+					class="sm:mt-20 mt-8 flex justify-center text-5xl font-extrabold text-white sm:text-7xl"
+				>
+					Fueled by {data.slug}
+				</h1>
+				
+			</div>
 		</div>
+		{#if carArray && carArray.length > 0}
+			<ul class=" flex flex-wrap justify-center p-5 sm:mt-0 mt-8">
+				{#each carArray as car}
+					<li>
+						<Card
+							model={car.model}
+							make={car.make}
+							mileage={car.mileage}
+							color={car.color}
+							fuel_type={car.fuel_type}
+							year_make={car.year_make}
+							price={car.price}
+							type={car.type}
+							transmission={car.gearbox}
+							registration={car.registration}
+							usage={car.usage}
+						/>
+					</li>
+				{/each}
+			</ul>
+		{:else}
+			<p>No cars available.</p>
+		{/if}
 	</div>
-	{#if carArray && carArray.length > 0}
-		<ul class=" flex flex-wrap justify-center p-5">
-			{#each carArray as car}
-				<li>
-					<Card
-						model={car.model}
-						make={car.make}
-						mileage={car.mileage}
-						color={car.color}
-						fuel_type={car.fuel_type}
-						year_make={car.year_make}
-						price={car.price}
-						type={car.type}
-						transmission={car.gearbox}
-						registration={car.registration}
-						usage={car.usage}
-					/>
-				</li>
-			{/each}
-		</ul>
-	{:else}
-		<p>No cars available.</p>
-	{/if}
 </div>
