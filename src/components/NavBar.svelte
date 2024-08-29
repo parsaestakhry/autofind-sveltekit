31e617d068117c1fd9717b41f5e5903ce26a503f
Parsa Estakhri <parsaestakhry03@gmail.com>
2024-08-29 11:09:20 +0330

    text-2xl and flex-1

--- src/components/NavBar.svelte
+++ src/components/NavBar.svelte
@@ -10,46 +10,46 @@
 </script>
 
 <div class="navbar bg-orange-600">
-	<div class="flex space-x-5" >
+	<div class="flex-1 space-x-5" >
 		<a data-sveltekit-reload href="/" class="btn btn-ghost ml-4 text-2xl font-extrabold text-slate-800 "
 			>caruns (beta)
 		</a>
 		<a
 			data-sveltekit-reload
 			href="/cars/usage/new"
-			class=" ml-4 hidden  text-slate-800 text-xl font-extrabold sm:flex no-underline hover:underline hover:underline-offset-4 "
+			class=" ml-4 hidden  text-slate-800 text-2xl font-extrabold sm:flex no-underline hover:underline hover:underline-offset-4 "
 			>New
 		</a>
 		<a
 			data-sveltekit-reload
 			href="/cars/usage/used"
-			class=" ml-4 hidden  text-slate-800 text-xl font-extrabold sm:flex hover:underline hover:underline-offset-4 "
+			class=" ml-4 hidden  text-slate-800 text-2xl font-extrabold sm:flex hover:underline hover:underline-offset-4 "
 			>Used
 		</a>
 		<a
 			data-sveltekit-reload
 			href="/cars/fuel-types/electric"
-			class=" ml-4 hidden  text-slate-800 text-xl font-extrabold sm:flex hover:underline hover:underline-offset-4 "
+			class=" ml-4 hidden  text-slate-800 text-2xl font-extrabold sm:flex hover:underline hover:underline-offset-4 "
 			>Electric
 		</a>
 		{#if username !== ''}
 			<a
 				data-sveltekit-reload
 				href="/user/{username}"
-				class=" ml-4 hidden  text-slate-800 text-xl font-extrabold sm:flex hover:underline hover:underline-offset-4 "
+				class=" ml-4 hidden  text-slate-800 text-2xl font-extrabold sm:flex hover:underline hover:underline-offset-4 "
 				>Account
 			</a>
 
 			<a
 				href="/sign-out"
 				data-sveltekit-reload
-				class=" ml-4 hidden  text-slate-800 text-xl font-extrabold sm:flex hover:underline hover:underline-offset-4 "
+				class=" ml-4 hidden  text-slate-800 text-2xl font-extrabold sm:flex hover:underline hover:underline-offset-4 "
 				>SignOut 
 			</a>
 			<a
 				href="/saved"
 				data-sveltekit-reload
-				class=" ml-4 hidden  text-slate-800 text-xl font-extrabold sm:flex hover:underline hover:underline-offset-4 "
+				class=" ml-4 hidden  text-slate-800 text-2xl font-extrabold sm:flex hover:underline hover:underline-offset-4 "
 				>Saved 
 			</a>
 		{:else}
