d0c4fc25245959cbe0b861a58ecc6aa9c4f36d6f
Parsa Estakhri <parsaestakhry03@gmail.com>
2024-08-29 10:53:00 +0330

    added sorry text

--- src/routes/cars/fuel-types/[slug]/+page.svelte
+++ src/routes/cars/fuel-types/[slug]/+page.svelte
@@ -12,17 +12,16 @@
 <div class="">
 	<div class="min-h-screen bg-gray-900">
 		<div class="relative sm:h-96">
-			<div class="  text-center font-bold flex justify-center ">
+			<div class="  flex justify-center text-center font-bold">
 				<h1
-					class="sm:mt-20 mt-8 flex justify-center text-5xl font-extrabold text-white sm:text-7xl"
+					class="mt-8 flex justify-center text-5xl font-extrabold text-white sm:mt-20 sm:text-7xl"
 				>
 					Fueled by {data.slug}
 				</h1>
-				
 			</div>
 		</div>
 		{#if carArray && carArray.length > 0}
-			<ul class=" flex flex-wrap justify-center p-5 sm:mt-0 mt-8">
+			<ul class=" mt-8 flex flex-wrap justify-center p-5 sm:mt-0">
 				{#each carArray as car}
 					<li>
 						<Card
@@ -42,7 +41,14 @@
 				{/each}
 			</ul>
 		{:else}
-			<p>No cars available.</p>
+			<div  class="flex justify-center">
+				<h4
+					class=" sm:mx-0 mx-20 mt-10 mb-4 sm:mt-5 max-w-2xl text-center text-3xl font-extrabold leading-none tracking-tight sm:text-wrap md:text-5xl xl:text-6xl dark:text-white"
+				>
+					Sorry we currently don't have any cars fueled by {data.slug}
+					<h4>:(</h4>
+				</h4>
+			</div>
 		{/if}
 	</div>
 </div>
