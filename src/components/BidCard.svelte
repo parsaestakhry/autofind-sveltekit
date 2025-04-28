ac77479c0a8e59c608a635d828bde72bd860c31d
Parsa Estakhri <parsaestakhry03@gmail.com>
2025-04-28 17:27:32 +0330

    fix: correct typo in bid amount display

--- src/components/BidCard.svelte
+++ src/components/BidCard.svelte
@@ -7,5 +7,5 @@
 
 <div role="alert" class="alert bg-inherit text-slate-50 border-4 border-orange-600 font-extrabold text-xl mt-2 ">
     <Money size={30} weight='bold' />
-  <span>Bid by : {userBid} for Amount of  : {amount}$ </span>
+  <span>Bid by : {userBid} for Amount offffff  : {amount}$ </span>
 </div>
